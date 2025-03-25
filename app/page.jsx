a726937e163ea457bcf0ba98f403684ba8e481f5
RaidanRR <127461111+RaidanRR@users.noreply.github.com>
2025-03-26 00:44:17 +0700

    h-full

--- app/page.jsx
+++ app/page.jsx
@@ -54,21 +54,21 @@ export default function Home() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {posts.map((post, index) => (
-          <Link href={post.link} key={index} className="block group">
-            <div className="border rounded-lg shadow-md overflow-hidden hover:shadow-lg transition duration-300">
-              <img
-                src={post.image}
-                alt={post.title}
-                className="w-full h-48 object-cover group-hover:opacity-80 transition-opacity"
-              />
-              <div className="p-4">
-                <h2 className="text-xl font-bold mb-2 group-hover:text-blue-600 transition-colors">
-                  {post.title}
-                </h2>
-                <p className="text-gray-600">{post.description}</p>
-              </div>
+          <Link href={post.link} key={index} className="block h-full">
+          <div className="flex flex-col justify-between h-full border rounded-lg shadow-md overflow-hidden hover:shadow-lg transition duration-300 min-h-[300px]">
+            <img
+              src={post.image}
+              alt={post.title}
+              className="w-full h-48 object-cover group-hover:opacity-80 transition-opacity"
+            />
+            <div className="p-4 flex flex-col flex-grow">
+              <h2 className="text-xl font-bold mb-2 group-hover:text-blue-600 transition-colors">
+                {post.title}
+              </h2>
+              <p className="text-gray-600 flex-grow">{post.description}</p>
             </div>
-          </Link>
+          </div>
+        </Link>        
         ))}
       </div>
     </div>
